822a398dbdaa895f32f736b72245c6d45997078e
HasanAlii73 <ha1338967@gmail.com>
2026-04-25 03:02:50 +0300

    me commit

--- index.js
+++ index.js
@@ -1,48 +1,79 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
-const PORT = 8080;
+// const PORT = 8080;
+
+// const server = http.createServer((req, res) => {
+//     let filePath = '';
+    
+//     switch (req.url) {
+//         case '/':
+//             filePath = './index.html';
+//             break;
+//         case '/about':
+//             filePath = './about.html';
+//             break;
+//         case '/contact-me':
+//             filePath = './contact-me.html';
+//             break;
+//         default:
+//             filePath = './404.html';
+//     }
+    
+//     let contentType = 'text/html';
+    
+
+//     fs.readFile(filePath, (err, content) => {
+//         if (err) {
+//             if (err.code === 'ENOENT') {
+//                 fs.readFile('./404.html', (err404, content404) => {
+//                     if (err404) {
+//                         res.writeHead(404, { 'Content-Type': 'text/html' });
+//                         res.end('<h1>404 - Page Not Found</h1>');
+//                     } else {
+//                         res.writeHead(404, { 'Content-Type': 'text/html' });
+//                         res.end(content404);
+//                     }
+//                 });
+//             } else {
+//                 res.writeHead(500);
+//                 res.end(`Server Error: ${err.code}`);
+//             }
+//         } else {
+//             res.writeHead(200, { 'Content-Type': contentType });
+//             res.end(content);
+//         }
+//     });
+// });
 
 const server = http.createServer((req, res) => {
-    let filePath = '';
+    console.log(`Received request for: ${req.url}`);
+    res.setHeader('Content-Type', 'text/html')
     
+    let path = './' 
     switch (req.url) {
         case '/':
-            filePath = './index.html';
-            break;
+             path += 'index.html'; break;
         case '/about':
-            filePath = './about.html';
-            break;
+             path += 'about.html'; break;
         case '/contact-me':
-            filePath = './contact-me.html';
-            break;
+             path += 'contact-me.html'; break;
         default:
-            filePath = './404.html';
+             path += '404.html'; break;
     }
-    
-    let contentType = 'text/html';
-    
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                fs.readFile('./404.html', (err404, content404) => {
-                    if (err404) {
-                        res.writeHead(404, { 'Content-Type': 'text/html' });
-                        res.end('<h1>404 - Page Not Found</h1>');
-                    } else {
-                        res.writeHead(404, { 'Content-Type': 'text/html' });
-                        res.end(content404);
-                    }
-                });
-            } else {
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content);
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.end()
         }
-    });
-});
+        else {
+            res.end(data)
+        }
+    })
+})
+
+server.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
+})
